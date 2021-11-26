46bab1e2b0ba7282d874eb5740cda12077ee8c98
Viacheslav Kozlov <slavick.cv@gmail.com>
2021-11-26 16:12:52 +0100

    fixed rename path and jimp resize

--- controllers/usersControllers/model/updAvatar.js
+++ controllers/usersControllers/model/updAvatar.js
@@ -3,25 +3,24 @@
 const fs = require("fs");
 const path = require("path");
 const { Unauthorized } = require("../../../helpers/errors.js");
-// const Jimp = require("jimp");
+const Jimp = require("jimp");
 
 const { usersServices } = require("../../../services");
 
 const updAvatar = async (req, res, next) => {
   const { _id } = req.user;
-  // console.log(_id);
   const { path: TMP_DIR, originalname } = req.file;
   const [fileExt] = originalname.split(".").reverse();
   const fileName = `${_id}.${fileExt}`;
-  const DEST_DIR = path.join(__dirname, "../../", "public/avatars", fileName);
-
+  const DEST_DIR = path.join(__dirname, "../../../", "public/avatars", fileName);
   if (!_id) {
     next(new Unauthorized("Not authorized"));
   }
 
   try {
-    // const fileName = await Jimp.read(TMP_DIR);
-    // await fileName.resize(250, 250).write(TMP_DIR);
+    await Jimp.read(TMP_DIR).then(originalname => {
+      return originalname.resize(250, 250).write(TMP_DIR);
+    });
     await fs.rename(TMP_DIR, DEST_DIR, () => {});
     const avatar = path.join("avatars", fileName);
     const data = await usersServices.updAvatarField(_id, avatar);
